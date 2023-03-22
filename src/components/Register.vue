c99d2abf3886aff2805bf7df8346d783257de2b4
Ryan Dewick <ryan.dewick@outlook.com>
2023-03-22 13:31:48 +0000

    added data and methods

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,48 +1,39 @@
 <template>
-<p><input type="text" placeholder="Email" v-model="email" /></p>
-<p><input type="password" placeholder="Password" v-model="password" /></p>
-<p><button @click="register">Submit</button></p>
-<p><button @click="signInWithGoogle">Sign In With Google</button></p>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password" /></p>
+  <p><button @click="register">Submit</button></p>
+  <p><button @click="signInWithGoogle">Sign In With Google</button></p>
 </template>
 
-
-
-
-<script setup>
-import { ref } from "vue";
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-const email = ref("");
-const password = ref("");
-const register = () => {
-
-    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+<script>
+// import { ref } from "vue";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    register() {
+      createUserWithEmailAndPassword(getAuth(), this.email, this.password)
         .then((data) => {
-            console.log("Registeration Done");
+          console.log("Registeration Done");
+          console.log(data);
         })
         .catch((error) => {
-            console.log("Error Registering")
-            alert(error.message);
-        })
-
+          console.log("Error Registering");
+          alert(error.message);
+        });
+    },
+  },
 };
-
-const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(getAuth(), provider)
-    .then((result) => {
-        console.log(result.user);
-    })
-    .catch((error) => {
-            //handle the error
-    });
-}
-
-
-
 </script>
-
-
-
-
 
 <style></style>
