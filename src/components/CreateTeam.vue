2878a07eca2b8c9a47bbb0b9d7b5f6605fca5f85
jdilkes1995 <joedilkes95@gmail.com>
2023-03-25 21:02:04 +0000

    added files

--- src/components/CreateTeam.vue
+++ src/components/CreateTeam.vue
@@ -1,3 +1,24 @@
-<template>
-  <p>hello</p>
-</template>
+<!-- 
+
+  A way to create teams
+    - Team name (feild)
+    - Users assigned to team name (Search function)
+    - Place holder for team 
+    - A way to add users to the team (Need to figure out a way to send confirmation!)
+    - Osrs usernames
+    - 
+
+
+ -->
+
+
+<template></template>
+
+<script>
+
+
+
+</script>
+
+<style></style>
+-
