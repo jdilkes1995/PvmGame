30bfee8638fc4fd22ca45716003b05b2b9897ee4
Ryan Dewick <ryan.dewick@outlook.com>
2023-03-25 17:54:07 +0000

    removed SignIn path and import in routes.js

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import Success from "@/components/Success.vue";
 import Register from "@/components/Register.vue";
-import SignIn from "@/components/SignIn.vue";
 import MyTeam from "@/components/MyTeam.vue";
 import CreateTeam from "@/components/CreateTeam.vue";
 import Play from "@/components/Play.vue";
@@ -22,7 +21,6 @@ const routes = [
       hideNavbar: true,
     },
   },
-  // { path: "/sign", component: SignIn },
 ];
 
 const router = createRouter({
