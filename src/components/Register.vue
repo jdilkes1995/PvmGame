98a24aa3b4d13ec50801063bf13e1707700b4d26
Ryan Dewick <ryan.dewick@outlook.com>
2023-03-25 17:56:43 +0000

    fixed register, when you now click register, it automatically signs in

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -61,6 +61,7 @@ export default {
           password: this.password,
         })
         .then(() => {
+          localStorage.setItem("isSignedIn", true);
           this.$router.push("/");
         })
         .catch((error) => {
